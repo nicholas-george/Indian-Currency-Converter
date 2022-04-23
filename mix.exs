94a62d836e82c3b3f17e8544b73a42d98b572546
Nicholas George <nicholas.george@greenclouds.in>
2022-04-24 05:28:01 +0530

    doc

--- mix.exs
+++ mix.exs
@@ -7,7 +7,11 @@ defmodule InrWord.MixProject do
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: description(),
+      package: package(),
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      source_url: "https://github.com/nicholas-george/Indian-Currency-Converter"
     ]
   end
 
@@ -22,9 +26,23 @@ defmodule InrWord.MixProject do
   defp deps do
     [
       {:ex_doc, "~> 0.27", only: :dev, runtime: false},
-      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.13", only: [:test]}
+    ]
+  end
+
+  defp description() do
+    "An utility to convert numbers into Indian Currency."
+  end
+
+  defp package() do
+    [
+      # This option is only needed when you don't want to use the OTP application name
+      name: "inr_word",
+      # These are the default files included in the package
+      files: ~w(lib .formatter.exs mix.exs README* LICENSE*),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/nicholas-george/Indian-Currency-Converter"}
     ]
   end
 end
